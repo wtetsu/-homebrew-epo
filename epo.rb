bbc2381b820a838de86e260e0084f00443ea98d6
wtetsu <tewatana@gmail.com>
2022-05-06 15:26:08 +0900

    v1.0.1

--- epo.rb
+++ epo.rb
@@ -1,8 +1,8 @@
 class Epo < Formula
   desc "Handy epoch converter"
   homepage "https://github.com/wtetsu/epo"
-  url "https://github.com/wtetsu/epo/releases/download/v1.0.0/epo_macos_amd_v1.0.0.zip"
-  sha256 "eff39bef9d2428b4a32dd8461ce9dfa2317137804e4d50980736a724c6ea40f1"
+  url "https://github.com/wtetsu/epo/releases/download/v1.0.1/epo_macos_amd_v1.0.1.zip"
+  sha256 "b2d740ddb38136bfab7ac9f2b0c0a36e85f6a7ea9d01302ab4d6c3a0966120a7"
   license "MIT"
 
   def install
